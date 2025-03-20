f009979618b8ff4c811e1d0c1ccca78029edc4ee
camposmm <mcamposdossantos@byupathway.edu>
2025-03-20 10:24:03 -0300

    week03

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -2,54 +2,23 @@ using System;
 
 class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
-        Random random = new Random();
-        bool playAgain = true;
+        // Test constructors
+        Fraction fraction1 = new Fraction();       // 1/1
+        Fraction fraction2 = new Fraction(5);       // 5/1
+        Fraction fraction3 = new Fraction(3, 4);    // 3/4
+        Fraction fraction4 = new Fraction(1, 3);   // 1/3
 
-        while (playAgain)
-        {
-            
-            int magicNumber = random.Next(1, 101);
-            int guessCount = 0;
-            int guess = 0;
+        // Test getters and setters
+        fraction1.SetNumerator(6);
+        fraction1.SetDenominator(7);
+        Console.WriteLine($"Fraction 1: {fraction1.GetFractionString()}"); // Should print 6/7
 
-            Console.WriteLine("Welcome to the Guess My Number game!");
-            Console.WriteLine("I have chosen a number between 1 and 100. Can you guess it?");
-
-           
-            while (guess != magicNumber)
-            {
-                Console.Write("What is your guess? ");
-                guess = int.Parse(Console.ReadLine());
-                guessCount++;
-
-                if (guess < magicNumber)
-                {
-                    Console.WriteLine("Higher");
-                }
-                else if (guess > magicNumber)
-                {
-                    Console.WriteLine("Lower");
-                }
-                else
-                {
-                    Console.WriteLine("You guessed it!");
-                }
-            }
-
-            Console.WriteLine($"It took you {guessCount} guesses.");
-
-            
-            Console.Write("Do you want to play again? (yes/no): ");
-            string playAgainResponse = Console.ReadLine().ToLower();
-
-            if (playAgainResponse != "yes")
-            {
-                playAgain = false;
-            }
-        }
-
-        Console.WriteLine("Thanks for playing! Goodbye!");
+        // Test representation methods
+        Console.WriteLine($"{fraction1.GetFractionString()} = {fraction1.GetDecimalValue()}");
+        Console.WriteLine($"{fraction2.GetFractionString()} = {fraction2.GetDecimalValue()}");
+        Console.WriteLine($"{fraction3.GetFractionString()} = {fraction3.GetDecimalValue()}");
+        Console.WriteLine($"{fraction4.GetFractionString()} = {fraction4.GetDecimalValue()}");
     }
 }
