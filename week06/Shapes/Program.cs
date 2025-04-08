e5a22045e63a537115c4247567862963a1981630
camposmm <mcamposdossantos@byupathway.edu>
2025-04-08 14:41:50 -0300

    week06

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,33 @@
+// Program.cs
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        // Test individual shapes
+        Square square = new Square("Red", 5);
+        Console.WriteLine($"Square - Color: {square.GetColor()}, Area: {square.GetArea()}");
+
+        Rectangle rectangle = new Rectangle("Blue", 4, 6);
+        Console.WriteLine($"Rectangle - Color: {rectangle.GetColor()}, Area: {rectangle.GetArea()}");
+
+        Circle circle = new Circle("Green", 3);
+        Console.WriteLine($"Circle - Color: {circle.GetColor()}, Area: {circle.GetArea()}");
+
+        // Create a list of shapes
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Square("Yellow", 2));
+        shapes.Add(new Rectangle("Purple", 3, 5));
+        shapes.Add(new Circle("Orange", 4));
+        shapes.Add(new Square("Black", 2.5));
+        shapes.Add(new Rectangle("White", 10, 2));
+
+        // Iterate through the list and display color and area
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"{shape.GetType().Name} - Color: {shape.GetColor()}, Area: {shape.GetArea():F2}");
+        }
     }
 }
